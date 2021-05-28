b62b96732392e48d67e785e7e0de1f5bd0d58347
DaveNincsItt <75878406+DaveNincsItt@users.noreply.github.com>
2021-05-28 21:52:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+const Discord = require("discord.js");
+const tokenfile = require("./tokenfile.json");
+const botconfig = require("./botconfig.json");
+const bot = new Discord.Client({disableEveryone: true});
+var weather = require('weather-js');
+const superagent = require('superagent');
+const randomPuppy = require('random-puppy');
+
+const fs = require("fs");
+const ms = require("ms");
+const money = require("./money.json");
+const { error } = require("console");
+const { attachCookies } = require("superagent");
+
+
+//////////////////////////////////////////////////////////
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+bot.categories = fs.readdirSync("./commands/");
+
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(bot)
+});
+
+bot.on("message", async message => {
+    let prefix = botconfig.prefix;
+
+    if(message.author.bot) return;
+    if(!message.guild) return;
+    if(!message.content.startsWith(prefix)) return;
+    if(!message.member) message.member = await message.guild.fetchMember(message)
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    if(cmd.length === 0) return;
+
+    let command = bot.commands.get(cmd);
+    if(!command) command = bot.commands.get(bot.aliases.get(cmd));
+
+    if(command)
+    command.run(bot, message, args);
+});
+
+//////////////////////////////////////////////////////////////////////////////////////
+ 
+
+
+let botname = "Cap Bot™"
+
+bot.on("ready", async() => {
+    console.log(`${bot.user.username} Online!`)
+
+    let státuszok = [
+        "Prefix: +",
+        "Developer: Dave_#6544"
+    ]
+
+    setInterval(function() {
+        let status = státuszok[Math.floor(Math.random()* státuszok.length)]
+
+        bot.user.setActivity(status, {type: "WATCHING"})
+    }, 3000)
+})
+
+bot.on("message", async message => {
+    let MessageArray = message.content.split(" ");
+    let cmd = MessageArray[0];
+    let args = MessageArray.slice(1);
+    let prefix = botconfig.prefix;
+
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+
+});
+
+
+
+
+bot.login(tokenfile.token);
